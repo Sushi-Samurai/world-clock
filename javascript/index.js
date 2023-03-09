64e45dc9b48197fbe86ece54b75eb47d9df64068
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2023-03-08 20:03:40 -0800

    Formatted the project with Javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  //San Diego
+  let sanDiegoElement = document.querySelector("#san-diego");
+  let sanDiegoDateElement = sanDiegoElement.querySelector(".date");
+  let sanDiegoTimeElement = sanDiegoElement.querySelector(".time");
+  let sanDiegoTime = moment().tz("US/Pacific");
 
+  sanDiegoDateElement.innerHTML = sanDiegoTime.format("MMMM Do YYYY");
+  sanDiegoTimeElement.innerHTML = sanDiegoTimeElement.innerHTML =
+    sanDiegoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
