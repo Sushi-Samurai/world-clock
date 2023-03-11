8cca35359f8200fb00e670b6da8bd69f7e3f72d4
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2023-03-10 21:52:53 -0800

    Added the current location

--- javascript/index.js
+++ javascript/index.js
@@ -27,6 +27,10 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
